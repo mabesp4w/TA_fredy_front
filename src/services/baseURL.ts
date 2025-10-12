0fa1a5a522f55e2a43858eeb6d0222bc8833801f
Mabesp4w <mabesp4w@gmail.com>
2025-10-12 10:24:11 +0900

    menambahkan url ML

--- src/services/baseURL.ts
+++ src/services/baseURL.ts
@@ -3,6 +3,7 @@
 import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const ML_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const url_auth = `${BASE_URL}/auth`;
 const url_api = `${BASE_URL}/api`;
 const url_crud = `${BASE_URL}/crud`;
@@ -18,6 +19,10 @@ const api = axios.create({
   baseURL: url_api,
 });
 
+const ml = axios.create({
+  baseURL: `${ML_URL}/api`,
+});
+
 const storage = axios.create({
   baseURL: url_storage,
 });
@@ -26,6 +31,7 @@ export {
   auth,
   crud,
   api,
+  ml,
   storage,
   BASE_URL,
   url_auth,
